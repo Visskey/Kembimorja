42ff93b3678cc979f392e833b5083c05d9602f41
visarahmeti <58608274+Visskey@users.noreply.github.com>
2022-07-29 15:35:58 +0200

    Inicalizimi i valutave qe do te kete kembimorja

--- Kembimorja.java
+++ Kembimorja.java
@@ -5,6 +5,26 @@ public class Kembimorja
 {
     public static void main(String[] args)
     { 
-    
+        double shuma;
+        double albanianlek, dollar, pound, euro, yen, swissfranc;
+        int zgjidhja;
+
+        DecimalFormat f = new DecimalFormat("##.##");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Following are the Choices:");
+        System.out.println("Enter 1: Albanian Lek");
+        System.out.println("Enter 2: US Dollars");
+        System.out.println("Enter 3: UK Pounds");
+        System.out.println("Enter 4: Euro");
+        System.out.println("Enter 5: Japan Yen");
+        System.out.println("Enter 5: Swiss Franc");
+
+        System.out.print("\nZgjedhni valuten qe deshironi te konvertoni: ");
+        zgjidhja = sc.nextInt();
+
+        System.out.println("Shtypeni shumen qe deshironi te konvertoni: ");
+        shuma = sc.nextFloat();
     }
 }
